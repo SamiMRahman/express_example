6c086b882b31fafeb94d1e6ec6c74f9cfdeadce9
Sami Rahman <sami101rahman@gmail.com>
2021-01-01 23:14:59 -0500

    added handler

---
+++ buildServer.js
@@ -0,0 +1,37 @@
+// building your own HTTP Server
+/**
+ * Express router takes in an HTTP request
+ * breaks it down into its method and path
+ * tries to find an endpoint (place to stop at)
+ * passes HTTP request off to that endpoint's handler
+ */
+const express = require("express") // include Express
+const app = express() // initialize Express
+
+/**
+ * code body
+ */
+
+ // Express endpoint handler that takes in HTTP request and sends back
+ function helloHandler(request, response) {
+
+    /** Query Parameters are the 'weird' stuff at end of a URL
+     * access query params in handler using 'request.query' object */
+     const name = request.query.name
+
+     /** passes HTTP protocol response status code to tell
+      * HTTP client how their request went */
+     response.status(200)
+     /** At end of every handler, we must send out response.
+      * Otherwise, basic rules of HTTP protocol are broken.
+      * 'response.send'
+      * To send out JSON (like for an API) -> 'response.json' */
+     response.send('Hello, ' + name + '!')
+
+ }
+ /** Link handler to Express router. Tell Express to run
+  * 'helloHandler' function for the 'GET/hello endpoint */
+ app.get('/hello', helloHandler)
+
+
+ app.listen(3000) // runs Express
